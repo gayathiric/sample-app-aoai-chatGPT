868aac0bd4c6c571b66cb48007e3b4683173d845
Gayathiri Chandrasekaran (US-2nd) <Gayathiri.Chandrasekaran@hsn.net>
2024-02-06 14:40:06 -0500

    customize chat window

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,7 +10,7 @@ import { isEmpty } from "lodash-es";
 import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Qurate from "../../assets/qurate.svg";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
@@ -624,12 +624,12 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={Contoso}
+                                    src={Qurate}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>IT Platforms Chatbot</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>I am configured to assist you with your troubleshooting!</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px" }} role="log">
